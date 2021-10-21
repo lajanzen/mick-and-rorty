b129422bee8cc36fb1af89efd0d8fe470113300c
lajanzen <lara-janzen@hotmail.com>
2021-10-21 17:21:31 +0200

    Add search bar

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import createCharacterCard from "./components/characterCard";
 import { createElement } from "./lib/elements";
 import fetchCharacters from "./lib/api";
 import "./style.css";
+import createSearchElement from "./components/searchBar";
 
 async function app() {
   const app = document.querySelector("#app");
@@ -16,16 +17,33 @@ async function app() {
       createElement("h1", {
         textContent: "Mick and Rorty API",
       }),
+      createSearchElement(handleSubmit),
     ]
   );
 
-  const characters = await fetchCharacters();
+  async function handleSubmit(searchQuery) {
+    const searchedCharacters = await fetchCharacters(
+      `https://rickandmortyapi.com/api/character/?name=${searchQuery}`
+    );
+
+    const searchedCharacterCards = searchedCharacters.map((character) =>
+      createCharacterCard(character)
+    );
+    main.innerHTML = "";
+    main.append(...searchedCharacterCards);
+
+    console.log(searchedCharacterCards);
+  }
+
+  const characters = await fetchCharacters(
+    "https://rickandmortyapi.com/api/character"
+  );
 
   const caracterCards = characters.map((character) =>
     createCharacterCard(character)
   );
 
-  const main = createElement("main", { className: "main" }, caracterCards);
+  const main = createElement("main", { className: "main" }, [...caracterCards]);
 
   app.append(header, main);
 }
